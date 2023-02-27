4723d40fcb819cde44782108c66c861cf8be5888
WisboyAk47 <sitouamu510@gmail.com>
2023-02-27 15:29:27 -0800

    6. Size project commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,16 @@
  *         various types
  *
  *return : Always 0 (success)
+ *
+ *Description: This program prints the size of various data types
+ *              on the computer on which it is compiled and run.
+ *              The return value of the main function is used to
+ *              indicate the status of the program execution, where
+ *              0 signifies success.
  */
 
 int main(void)
-{    
+{
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
 printf("Size of an int: %ld byte(s)\n", sizeof(int));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
