1f24dfd6527dc65831d71284b9222b0ebd9e4ebf
WisboyAk47 <sitouamu510@gmail.com>
2023-02-27 06:53:00 -0800

    5. Hello, printf commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints a line with the printf function
+ *
+ * Description: This program uses the printf function to
+ *              print a line of text to the standard output.
+ *
+ * Return: Always 0 (success)
+ *
+ * Description (return): The main function returns an integer value
+ *                       to indicate the status of the program after
+ *                       it has finished executing. A return value of
+ *                       0 indicates success, while any other value
+ *                       indicates an error or other problem.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
