240379813b41dc970d501446b8e31230d3e369fb
WisboyAk47 <sitouamu510@gmail.com>
2023-02-27 02:55:21 -0800

    Hello, puts project commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints a line with puts function
+ *
+ * Description: This program uses the puts function to print a line of text
+ *              to the standard output. The line includes escaped quotes
+ *              and a newline character.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
